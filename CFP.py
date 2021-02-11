2b080d5591298f52fbfedeadc1b960432394d46a
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-02-11 18:23:43 +0300

    fix class

--- CFP.py
+++ CFP.py
@@ -1,65 +1,60 @@
 import random
 
 
+def readFile(path):
+    with open(path) as f:
+        m, p = [int(num) for num in f.readline().split()]
+        matrix_mp = [[0] * p for i in range(m)]
+        for i in range(m):
+            list_parts = [int(num) for num in f.readline().split()]
+            machine = list_parts[0]
+            for part in list_parts[1:]:
+                matrix_mp[machine - 1][part - 1] = 1
+        return matrix_mp
+
+
 class GeneralVND:
+
     def __init__(self, path):
-        self.matrix = self.readFile(path)
+        self.matrix = readFile(path)
         self.zeroes = self.countZeroes()
         self.ones = self.countOnes()
         self.solution = [[]]
+        self.machines = len(self.matrix)
+        self.parts = len(self.matrix[0])
 
-    def readFile(path):
-        with open(path) as f:
-            m, p = [int(num) for num in f.readline().split()]
-            matrix_mp = [[0] * p for i in range(m)]
-            for i in range(m):
-                list_parts = [int(num) for num in f.readline().split()]
-                machine = list_parts[0]
-                for part in list_parts[1:]:
-                    matrix_mp[machine - 1][part - 1] = 1
-            return matrix_mp
-
-
-    def solve(matrix_mp):
-        return 0
-
-
-    def efficiency(matrix_mp, solution):
+    def efficiency(self):
         zeroes = 0
         ones = 0
-        for i in range(len(matrix_mp)):
-            for j in range(len(matrix_mp[0])):
-                if matrix[i][j] == 1:
+        for i in range(len(self.matrix)):
+            for j in range(len(self.matrix[0])):
+                if self.matrix[i][j] == 1:
                     zeroes += 1
                 else:
                     ones += 1
-
         return 0
 
-
-    def generateConfigsUniform(machines, parts):
+    def generateConfigsUniform(self, machines, parts):
         min_dimension = min(machines, parts)
         cells_number = random.randint(1, min_dimension)
-        m_decomposition = decomposition(cells_number, machines)
-        p_decomposition = decomposition(cells_number, parts)
-        solution = [splitting(m_decomposition, machines), splitting(p_decomposition, parts)]
+        m_decomposition = self.decomposition(cells_number, machines)
+        p_decomposition = self.decomposition(cells_number, parts)
+        solution = [self.splitting(self, m_decomposition, machines), self.splitting(self, p_decomposition, parts)]
         return solution
 
-
     # рандомно раскладываем по ячейкам станки/детали
-    def splitting(decomposition, length):
+    def splitting(self, decomp, length):
         cells_num = [0]*length
-        for i in range(0, len(decomposition)):
-            for j in range(1, decomposition[i] + 1):
+        for i in range(0, len(decomp)):
+            for j in range(1, decomp[i] + 1):
                 part = random.randint(0, length - 1)
                 while cells_num[part] != 0:
                     part = random.randint(0, length - 1)
                 cells_num[part] = i + 1
         return cells_num
 
-
     # разложение на terms_number числа number
-    def Decomposition(terms_number, number):
+    def decomposition(self, terms_number, number):
         size_configs = [number]
         for i in range(terms_number - 1):
             if max(size_configs) == 1:
@@ -73,5 +68,4 @@ class GeneralVND:
         return size_configs
 
 
-matrix = ReadFile('test.txt')
-print(GenerateConfigsUniform(len(matrix), len(matrix[0])))
+#print(GenerateConfigsUniform(len(matrix), len(matrix[0])))
